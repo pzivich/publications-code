abc9299eacb125d35de7d4ac4667746e20131516
pzivich <pzivich@live.unc.edu>
2023-05-21 09:47:28 -0400

    Updating sims for TransportNoPositivity

--- TransportNoPositivity/run_estimators.py
+++ TransportNoPositivity/run_estimators.py
@@ -17,6 +17,8 @@ from helper import synthesis_g_computation, synthesis_ipw
 np.random.seed(80951342)        # Setting seed
 runs = 2000                     # Number of simulation iterations
 mc_iterations = 5000            # Number of iterations for Monte Carlo procedure in EACH simulation iteration
+n1, n0 = 1000, 1000             # Sample sizes for available data (clinic, trial)
+ns = 2000                       # Sample size of the secret trial (as stand-in for background info)
 n_cpus = 15                     # Number of CPUs available to use (done on my desktop)
 
 
@@ -66,6 +68,13 @@ if __name__ == "__main__":                   # Call in case running on windows (
                                     'bias_pr3_g', 'cld_pr3_g', 'cover_pr3_g', 'bias_pr3_w', 'cld_pr3_w', 'cover_pr3_w',
                                     'bias_pr4_g', 'cld_pr4_g', 'cover_pr4_g', 'bias_pr4_w', 'cld_pr4_w', 'cover_pr4_w',
                                     'bias_pr5_g', 'cld_pr5_g', 'cover_pr5_g', 'bias_pr5_w', 'cld_pr5_w', 'cover_pr5_w',
+                                    'bias_pr6_g', 'cld_pr6_g', 'cover_pr6_g', 'bias_pr6_w', 'cld_pr6_w', 'cover_pr6_w',
+                                    'bias_pr7_g', 'cld_pr7_g', 'cover_pr7_g', 'bias_pr7_w', 'cld_pr7_w', 'cover_pr7_w',
+                                    'bias_pr8_g', 'cld_pr8_g', 'cover_pr8_g', 'bias_pr8_w', 'cld_pr8_w', 'cover_pr8_w',
+                                    'bias_pr9_g', 'cld_pr9_g', 'cover_pr9_g', 'bias_pr9_w', 'cld_pr9_w', 'cover_pr9_w',
+                                    'bias_pr10_g', 'cld_pr10_g', 'cover_pr10_g', 'bias_pr10_w', 'cld_pr10_w', 'cover_pr10_w',
+                                    'bias_pr11_g', 'cld_pr11_g', 'cover_pr11_g', 'bias_pr11_w', 'cld_pr11_w', 'cover_pr11_w',
+                                    'bias_pr12_g', 'cld_pr12_g', 'cover_pr12_g', 'bias_pr12_w', 'cld_pr12_w', 'cover_pr12_w',
                                     ])
 
     # Running the simulation
@@ -75,7 +84,7 @@ if __name__ == "__main__":                   # Call in case running on windows (
 
         ##########################################
         # Generate data
-        d = generate_data(n1=1000, n0=1000)
+        d = generate_data(n1=n1, n0=n0)
 
         ##########################################
         # Restrict the Target Population
@@ -143,9 +152,7 @@ if __name__ == "__main__":                   # Call in case running on windows (
             row.append(0)
 
         ##########################################
-        # Stat-Sim Synthesis
-        cond_params = generate_background_info(marginal=False)
-        marg_params = generate_background_info(marginal=True)
+        # Stat-Sim Synthesis -- fixed inputs
 
         # Scenario 1: Strict Null
         point, ci = synthesis_g_computation(data=d, mc_iters=mc_iterations, setup=1, n_cpus=n_cpus)
@@ -155,6 +162,7 @@ if __name__ == "__main__":                   # Call in case running on windows (
             row.append(1)
         else:
             row.append(0)
+
         point, ci = synthesis_ipw(data=d, mc_iters=mc_iterations, setup=1, n_cpus=n_cpus)
         row.append(point - truth)
         row.append(ci[1] - ci[0])
@@ -171,6 +179,7 @@ if __name__ == "__main__":                   # Call in case running on windows (
             row.append(1)
         else:
             row.append(0)
+
         point, ci = synthesis_ipw(data=d, mc_iters=mc_iterations, setup=2, n_cpus=n_cpus)
         row.append(point - truth)
         row.append(ci[1] - ci[0])
@@ -178,6 +187,11 @@ if __name__ == "__main__":                   # Call in case running on windows (
             row.append(1)
         else:
             row.append(0)
+
+        ##########################################
+        # Stat-Sim Synthesis -- dynamic inputs
+        cond_params = generate_background_info(marginal=False, n_secret=ns)
+        marg_params = generate_background_info(marginal=True, n_secret=ns)
 
         # Scenario 3: Accurate
         point, ci = synthesis_g_computation(data=d, mc_iters=mc_iterations, setup=3, n_cpus=n_cpus,
@@ -188,6 +202,7 @@ if __name__ == "__main__":                   # Call in case running on windows (
             row.append(1)
         else:
             row.append(0)
+
         point, ci = synthesis_ipw(data=d, mc_iters=mc_iterations, setup=3, n_cpus=n_cpus,
                                   mu=marg_params[0], cov=marg_params[1])
         row.append(point - truth)
@@ -206,6 +221,7 @@ if __name__ == "__main__":                   # Call in case running on windows (
             row.append(1)
         else:
             row.append(0)
+
         point, ci = synthesis_ipw(data=d, mc_iters=mc_iterations, setup=4, n_cpus=n_cpus,
                                   mu=-1*np.array(marg_params[0]), cov=marg_params[1])
         row.append(point - truth)
@@ -224,7 +240,152 @@ if __name__ == "__main__":                   # Call in case running on windows (
             row.append(1)
         else:
             row.append(0)
+
         point, ci = synthesis_ipw(data=d, mc_iters=mc_iterations, setup=5, n_cpus=n_cpus,
+                                  mu=marg_params[0], cov=marg_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        # Scenario 6: Flipped Marginal and Conditional Parameters
+        point, ci = synthesis_g_computation(data=d, mc_iters=mc_iterations, setup=3, n_cpus=n_cpus,
+                                            mu=marg_params[0], cov=marg_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        point, ci = synthesis_ipw(data=d, mc_iters=mc_iterations, setup=3, n_cpus=n_cpus,
+                                  mu=cond_params[0], cov=cond_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        # Scenario 7: Different Age Distribution in Secret Trial
+        cond_params = generate_background_info(marginal=False, n_secret=ns, third_population=True)
+        marg_params = generate_background_info(marginal=True, n_secret=ns, third_population=True)
+
+        point, ci = synthesis_g_computation(data=d, mc_iters=mc_iterations, setup=3, n_cpus=n_cpus,
+                                            mu=cond_params[0], cov=cond_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        point, ci = synthesis_ipw(data=d, mc_iters=mc_iterations, setup=3, n_cpus=n_cpus,
+                                  mu=marg_params[0], cov=marg_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        ##########################################
+        # Stat-Sim Synthesis -- dynamic inputs 2*N
+        cond_params = generate_background_info(marginal=False, n_secret=ns*2)
+        marg_params = generate_background_info(marginal=True, n_secret=ns*2)
+
+        # Scenario 3B: Accurate
+        point, ci = synthesis_g_computation(data=d, mc_iters=mc_iterations, setup=3, n_cpus=n_cpus,
+                                            mu=cond_params[0], cov=cond_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        point, ci = synthesis_ipw(data=d, mc_iters=mc_iterations, setup=3, n_cpus=n_cpus,
+                                  mu=marg_params[0], cov=marg_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        # Scenario 4B: Inaccurate
+        point, ci = synthesis_g_computation(data=d, mc_iters=mc_iterations, setup=4, n_cpus=n_cpus,
+                                            mu=-1*np.array(cond_params[0]), cov=cond_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        point, ci = synthesis_ipw(data=d, mc_iters=mc_iterations, setup=4, n_cpus=n_cpus,
+                                  mu=-1*np.array(marg_params[0]), cov=marg_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        # Scenario 5B: Accurate with Covariance
+        point, ci = synthesis_g_computation(data=d, mc_iters=mc_iterations, setup=5, n_cpus=n_cpus,
+                                            mu=cond_params[0], cov=cond_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        point, ci = synthesis_ipw(data=d, mc_iters=mc_iterations, setup=5, n_cpus=n_cpus,
+                                  mu=marg_params[0], cov=marg_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        # Scenario 6B: Flipped Marginal and Conditional Parameters
+        point, ci = synthesis_g_computation(data=d, mc_iters=mc_iterations, setup=3, n_cpus=n_cpus,
+                                            mu=marg_params[0], cov=marg_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        point, ci = synthesis_ipw(data=d, mc_iters=mc_iterations, setup=3, n_cpus=n_cpus,
+                                  mu=cond_params[0], cov=cond_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        # Scenario 7B: Different Age Distribution in Secret Trial
+        cond_params = generate_background_info(marginal=False, n_secret=ns*2, third_population=True)
+        marg_params = generate_background_info(marginal=True, n_secret=ns*2, third_population=True)
+
+        point, ci = synthesis_g_computation(data=d, mc_iters=mc_iterations, setup=3, n_cpus=n_cpus,
+                                            mu=cond_params[0], cov=cond_params[1])
+        row.append(point - truth)
+        row.append(ci[1] - ci[0])
+        if ci[0] < truth < ci[1]:
+            row.append(1)
+        else:
+            row.append(0)
+
+        point, ci = synthesis_ipw(data=d, mc_iters=mc_iterations, setup=3, n_cpus=n_cpus,
                                   mu=marg_params[0], cov=marg_params[1])
         row.append(point - truth)
         row.append(ci[1] - ci[0])
@@ -239,4 +400,4 @@ if __name__ == "__main__":                   # Call in case running on windows (
 
     ##########################################
     # Saving Resuts upon Completion
-    results.to_csv("sim_results.csv", index=False)
+    results.to_csv("sim_results_n1-"+str(n1)+"_n0-"+str(n0)+".csv", index=False)
